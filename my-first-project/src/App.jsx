a1b3d23cf082f22439f1dd54e6f77d7adeca2b94
thathsaraudari <105314224+thathsaraudari@users.noreply.github.com>
2025-06-08 23:31:38 +0200

    completed

--- my-first-project/src/App.jsx
+++ my-first-project/src/App.jsx
@@ -5,31 +5,31 @@ import menu from './assets/menu-icon.png';
 
 function App() {
   return (
-    <>
-      <h1>
-        IMAGINE A<br /> PLACE...
-      </h1>
-
-      <p>...where you can belong to a school club, a gaming group, or a<br />
-       worldwide art community. Where just you and a handful of<br />
-        friends can spend time together. A place that makes it easy to<br />
-         talk every day and hang out more often.
-      </p>
-      <div className="image-section">
+    <div className="hero">
+      <div className="background-div">
         <img src={background} alt="discord-background" className="background-img" />
-        <img src={logo} alt="discord-logo-white" className="logo-img" />
-        <img src={menu} alt="menu-icon" className="menu-img" />
       </div>
-      
-      <div className="buttons">
+
+      <img src={logo} alt="discord-logo-white" className="logo-img" />
+      <img src={menu} alt="menu-icon" className="menu-img" />
+
+      <div className="hero-content">
+        <h1>
+          IMAGINE A<br /> PLACE...
+        </h1>
+        <p>
+          ...where you can belong to a school club, a gaming group, or a<br />
+          worldwide art community. Where just you and a handful of<br />
+          friends can spend time together. A place that makes it easy to<br />
+          talk every day and hang out more often.
+        </p>
+        <div className="buttons">
           <button className="btn-light">Download for Mac</button>
           <button className="btn-dark">Open Discord in your browser</button>
+        </div>
       </div>
-
-    </>
+    </div>
   );
 }
 
 export default App;
-
-
